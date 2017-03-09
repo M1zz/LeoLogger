3c642e05d49aa288a98860a1284c29d291de46ad
M1zz <mizzking75@gmail.com>
2017-03-09 21:32:37 +0900

    Fitlog v.1.0.0
    The first version of fitlog

---
+++ src/com/leeo/fitlog/fitlogMain.java
@@ -0,0 +1,14 @@
+package com.leeo.fitlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class fitlogMain extends JavaPlugin{
+
+    public void onEnable(){
+	    System.out.println("[Fitlog] fitlog v.1.0.0 Plugin is Enable.");
+    }
+    
+    public void onDisable(){
+        System.out.println("[Fitlog] fitlog v.1.0.0 Plugin is Disable.");
+    }
+}
